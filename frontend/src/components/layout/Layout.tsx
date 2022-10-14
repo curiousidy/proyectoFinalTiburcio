c6b75c57b047a2fdf92c88871550edec02a5469b
¨Idaira <idaira.aleman@bitboxonline.com>
2022-10-14 15:38:22 +0100

    :tada: the code of the search screen has been implemented. Pending to polish the filter

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -14,29 +14,29 @@ const Layout : React.FC<LayoutInterface> = ({children}:LayoutInterface) => {
     <>
     
     <IonHeader class='ion-no-border'>
-    <IonToolbar>
-      <img className='logo' src='/assets/logo.png' alt='logoFilmFlix'/>
+      <IonToolbar>
+        <img className='logo' src='/assets/logo.png' alt='logoFilmFlix'/>
+        
+        <IonRow class='ion-justify-content-center ion-text-center'>
+          <IonCol size="4" class='ion-text-rigth'>
+            TV Show
+          </IonCol>
+
+          <IonCol size="4">
+            Movies
+          </IonCol>
       
-      <IonRow class='ion-justify-content-center ion-text-center'>
-        <IonCol size="4" class='ion-text-rigth'>
-          TV Show
-        </IonCol>
+          <IonCol size='4'/*TODO ONCLICK*/>
+            Categories <IonIcon icon={caretDownOutline}></IonIcon>
+          </IonCol>
 
-        <IonCol size="4">
-          Movies
-        </IonCol>
-    
-        <IonCol size='4'/*TODO ONCLICK*/>
-          Categories <IonIcon icon={caretDownOutline}></IonIcon>
-        </IonCol>
+        </IonRow>
+      </IonToolbar>
+    </IonHeader>
 
-      </IonRow>
-    </IonToolbar>
-  </IonHeader>
-
-  <IonContent>
-    {children}
-  </IonContent>
+    <IonContent>
+      {children}
+    </IonContent>
 
 		
   </>
--- frontend/src/pages/search/search.tsx
+++ frontend/src/pages/search/search.tsx
@@ -1,32 +1,53 @@
 import {IonContent, IonItem, IonList, IonPage, IonSearchbar, IonTitle, IonToolbar} from '@ionic/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import { fecthMovie } from '../../services/api';
 
-const Search: React.FC = () => {
+const Search: React.FC = (): JSX.Element => {
+  // const data = ['Amsterdam', 'Buenos Aires', 'Cairo', 'Geneva', 'Hong Kong', 'Istanbul', 'London', 'Madrid', 'New York', 'Panama City'];
+  // let [results, setResults] = useState([...data]);
+ 
+  let [results, setResults] = useState<any[]>([]);
 
-  const data = ['Amsterdam', 'Buenos Aires', 'Cairo', 'Geneva', 'Hong Kong', 'Istanbul', 'London', 'Madrid', 'New York', 'Panama City'];
-  let [results, setResults] = useState([...data]);
-
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    fecthMovie()
+    .then((response)=>{
+       console.log(response.results);
+       setResults(response.results);
+    });
+    
+  }, [])
+  
   
   const handleChange = (ev: Event) => {
+    
     let query = "";
     const target = ev.target as HTMLIonSearchbarElement;
     if (target) query = target.value!.toLowerCase();
-
-    setResults(data.filter(d => d.toLowerCase().indexOf(query) > -1));
+   console.log(results);
+   
+    
+    setResults(results.filter((d:any) => d.title.toLowerCase().indexOf(query) > -1));
+    
   }
-
+  
+  
   return (
-    <IonPage>
-         <IonToolbar>
+    
+     <IonPage>
+        <IonToolbar>
           <IonTitle className='title'>Search Film</IonTitle>
         </IonToolbar>
         <IonToolbar className='searchBar'>
-          <IonSearchbar debounce={200} onIonChange={(ev) => handleChange(ev)}/>
+          <IonSearchbar debounce={200} 
+          onIonChange={(ev) => handleChange(ev)}
+          />
         </IonToolbar>
         <IonContent fullscreen={true} className='ion-padding'>
             <IonList>
               { results.map(result => (
-                <IonItem>{ result }</IonItem>
+                <IonItem>{ result.title }</IonItem>
               ))}
             </IonList>
         </IonContent>
